a2239ee75d6d8715ae8af44531cfcd9c0f5fe19d
KimDoubleB <tree9295@gmail.com>
2020-06-03 16:28:13 +0900

    Add API of SELL

--- server/todayHouse/models/sell.js
+++ server/todayHouse/models/sell.js
@@ -6,37 +6,90 @@ const sell = {
         const query = `SELECT * FROM ${table} WHERE sellIdx = "${id}"`;
         try {
             const result = await pool.queryParam(query);
+            let result2;
+            for (res of result) {
+                // img 가져오기
+                result2 = await sell.getSellImages(res.sellIdx);
+                res['imgs'] = result2;
+
+                // review 가져오기
+                result2 = await sell.getReviews(res.sellIdx);
+                res['reviews'] = result2;
+            }
             return result;
         } catch (err) {
             throw err;
         }
-
     },
     getAllList: async () => {
-        let query = `SELECT * FROM Sell`;
+        const query = `SELECT * FROM Sell`;
         try {
             const result = await pool.queryParam(query);
             let result2;
             for (res of result) {
                 // img 가져오기
-                query = `SELECT imgUrl FROM SellImage WHERE Sell_sellIdx = ${res.sellIdx}`;
-                result2 = await pool.queryParam(query);
+                result2 = await sell.getSellImages(res.sellIdx);
                 res['imgs'] = result2;
 
                 // review 가져오기
-                query = `SELECT imgUrl, profileImgUrl, name, createdAt, content 
-                FROM ( SELECT imgUrl, createdAt, content, User_useridx FROM Review WHERE Sell_sellidx = ${res.sellIdx} ) AS R
-                INNER JOIN User
-                WHERE User.userIdx = R.User_useridx`;
-                result2 = await pool.queryParam(query);
+                result2 = await sell.getReviews(res.sellIdx);
                 res['reviews'] = result2;
             }
-
             return result;
         } catch (err) {
             throw err;
         }
     },
+    getBanner: async () => {
+        const cnt = await sell.getSellCount();
+        randomNum = Math.floor(Math.random() * cnt + 1);
+        console.log(randomNum);
+        const query = `SELECT sellIdx, company, discount FROM ${table} WHERE sellIdx = "${randomNum}"`;
+        try {
+            const result = await pool.queryParam(query);
+            console.log(result);
+            let result2;
+            for (res of result) {
+                // img 가져오기
+                console.log(res);
+                result2 = await sell.getSellImages(res.sellIdx);
+                res['imgUrl'] = result2[0].imgUrl;
+            }
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    },
+    getSellImages: async (sellIdx) => {
+        const query = `SELECT imgUrl FROM SellImage WHERE Sell_sellIdx = ${sellIdx}`;
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    },
+    getReviews: async (sellIdx) => {
+        const query = `SELECT imgUrl, profileImgUrl, name, createdAt, content 
+        FROM ( SELECT imgUrl, createdAt, content, User_useridx FROM Review WHERE Sell_sellidx = ${sellIdx} ) AS R
+        INNER JOIN User
+        WHERE User.userIdx = R.User_useridx`;
+        try {
+            const result = await pool.queryParam(query);
+            return result;
+        } catch (err) {
+            throw err;
+        }
+    },
+    getSellCount: async () =>{
+        const query = `SELECT COUNT(*) AS cnt FROM Sell`;
+        try{
+            const result = await pool.queryParam(query);
+            return result[0].cnt;
+        } catch (err){
+            throw err;
+        }
+    }
 }
 
 module.exports = sell;
--- server/todayHouse/routes/sell.js
+++ server/todayHouse/routes/sell.js
@@ -13,5 +13,21 @@ router.get('/', async (req, res) => {
     .send(util.success(statusCode.OK, resMessage.SELL_SUCCESS, sell));
 });
 
+router.get('/main', async (req, res) => {
+  const sell = await SellModel.getBanner();
+  return res.status(statusCode.OK)
+    .send(util.success(statusCode.OK, resMessage.SELL_SUCCESS, sell));
+});
+
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
+  const sell = await SellModel.getList(id);
+  if (sell.length > 0) {
+    return res.status(statusCode.OK)
+      .send(util.success(statusCode.OK, resMessage.SELL_SUCCESS, sell));
+  }
+  return res.status(statusCode.NOT_FOUND)
+    .send(util.fail(statusCode.NOT_FOUND, resMessage.SELL_FAIL));
+});
 
 module.exports = router;
