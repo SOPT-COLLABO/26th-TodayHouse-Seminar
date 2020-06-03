d3de062d4f2d7e9886327291b6216a20f4f53bf7
KimDoubleB <tree9295@gmail.com>
2020-06-03 14:53:58 +0900

    Update response messages

--- server/todayHouse/modules/responseMessage.js
+++ server/todayHouse/modules/responseMessage.js
@@ -3,14 +3,14 @@ module.exports = {
     OUT_OF_VALUE: "파라미터 값이 잘못되었습니다",
 
     // SELL 게시글
-    SELL_SUCCESS: "게시글 조회 성공",
-    SELL_FAIL: "게시글 조회 실패",
-    SELL_CREATE_SUCCESS: "게시글 생성 성공",
-    SELL_CREATE_FAIL: "게시글 생성 실패",
-    SELL_UPDATE_SUCCESS: "게시글 수정 성공",
-    SELL_UPDATE_FAIL: "게시글 수정 실패",
-    SELL_DELETE_SUCCESS: "게시글 삭제 성공",
-    SELL_DELETE_FAIL: "게시글 삭제 실패",
+    SELL_SUCCESS: "판매글 조회 성공",
+    SELL_FAIL: "판매글 조회 실패",
+    SELL_CREATE_SUCCESS: "판매글 생성 성공",
+    SELL_CREATE_FAIL: "판매글 생성 실패",
+    SELL_UPDATE_SUCCESS: "판매글 수정 성공",
+    SELL_UPDATE_FAIL: "판매글 수정 실패",
+    SELL_DELETE_SUCCESS: "판매글 삭제 성공",
+    SELL_DELETE_FAIL: "판매글 삭제 실패",
     
 
     // POST 게시글
