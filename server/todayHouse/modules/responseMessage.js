35963df29a14379c259c889c31528fb7e50e7213
Um seoyoung <tjdud0123@naver.com>
2020-06-04 02:22:48 +0900

    post관련 라우트 작성

--- server/todayHouse/modules/responseMessage.js
+++ server/todayHouse/modules/responseMessage.js
@@ -11,19 +11,21 @@ module.exports = {
     SELL_UPDATE_FAIL: "판매글 수정 실패",
     SELL_DELETE_SUCCESS: "판매글 삭제 성공",
     SELL_DELETE_FAIL: "판매글 삭제 실패",
-    
+
 
     // POST 게시글
-    POST_SUCCESS: "게시글 조회 성공",
+    ALL_POST_SUCCESS: "가구 포스트 불러오기 성공",
+    POPULAR_PHOTOS_SUCCESS: "인기사진 리스트 불러오기 성공",
+    STORY_SUCCESS: "메인화면 스토리 리스트 불러오기 성공",
     POST_FAIL: "게시글 조회 실패",
-    POST_CREATE_SUCCESS: "게시글 생성 성공",
-    POST_CREATE_FAIL: "게시글 생성 실패",
-    POST_UPDATE_SUCCESS: "게시글 수정 성공",
-    POST_UPDATE_FAIL: "게시글 수정 실패",
-    POST_DELETE_SUCCESS: "게시글 삭제 성공",
-    POST_DELETE_FAIL: "게시글 삭제 실패",
-    
+    // POST_CREATE_SUCCESS: "게시글 생성 성공",
+    // POST_CREATE_FAIL: "게시글 생성 실패",
+    // POST_UPDATE_SUCCESS: "게시글 수정 성공",
+    // POST_UPDATE_FAIL: "게시글 수정 실패",
+    // POST_DELETE_SUCCESS: "게시글 삭제 성공",
+    // POST_DELETE_FAIL: "게시글 삭제 실패",
 
-    
+
+
     DB_ERROR: "DB 오류"
 };
